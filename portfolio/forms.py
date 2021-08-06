6192e34a1b68dfce5940438c7311ca189257ee9e
Shumayl <83513359+mshumayl@users.noreply.github.com>
2021-08-06 17:18:40 +0800

    try fix model

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -1,6 +1,14 @@
 from django import forms 
 from .models import Post, Category
 
+#QuerySet object - selection for category
+choices = Category.objects.all().values_list('name', 'name')
+
+# #Fill QuerySet object into a Python list
+# choice_list = []
+# for item in choices:
+#     choice_list.append(item)
+
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
@@ -26,11 +34,4 @@ class EditForm(forms.ModelForm):
             'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Edit your content'}),
             'snippet': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Provide a snippet for your post'}),
         }
-
-#QuerySet object - selection for category
-choices = Category.objects.all().values_list('name', 'name')
-
-#Fill QuerySet object into a Python list
-choice_list = []
-for item in choices:
-    choice_list.append(item)
+        
