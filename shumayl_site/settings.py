54d82d9baf0c6d7edcdac27e9f440817e739c3de
Shumayl <83513359+mshumayl@users.noreply.github.com>
2021-08-07 00:09:58 +0800

    collecstatic for ckeditor into staticfiles

--- shumayl_site/settings.py
+++ shumayl_site/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-CKEDITOR_BASEPATH = "staticfiles/ckeditor"
+CKEDITOR_BASEPATH = "staticfiles/ckeditor/ckeditor/ckeditor.js"
 
 CKEDITOR_UPLOAD_PATH = 'uploads/'
 # CKEDITOR_CONFIGS = {
