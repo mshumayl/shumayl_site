d8fd9a81c142172b06d2b60271718cdc8c57747e
Shumayl <83513359+mshumayl@users.noreply.github.com>
2021-08-06 16:35:56 +0800

    try fix model

--- shumayl_site/settings.py
+++ shumayl_site/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
-import dj_database_url
+import dj-database-url
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
