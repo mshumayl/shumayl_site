af918dce7a6e422498be6fdf4b745c573bcfde32
Shumayl <83513359+mshumayl@users.noreply.github.com>
2021-08-06 17:23:13 +0800

    try fix model

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -4,20 +4,22 @@ from .models import Post, Category
 #QuerySet object - selection for category
 choices = Category.objects.all().values_list('name', 'name')
 
+# Fill QuerySet object into a Python list
+choice_list = []
+for item in choices:
+    choice_list.append(item)
+
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ("title", "author", "category", "body", "snippet", "header_image")
         
-        #Fill QuerySet object into a Python list
-        choice_list = []
-        for item in choices:
-            choice_list.append(item)
+
         
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Input your title here'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
-            'category': forms.Select(choices=choice_list, attrs={'class': 'form-control', 'placeholder': 'Select a tag'}),
+            # 'category': forms.Select(choices=choice_list, attrs={'class': 'form-control', 'placeholder': 'Select a tag'}),
             'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Type away!'}),
             'snippet': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Provide a snippet for your post'}),
             
