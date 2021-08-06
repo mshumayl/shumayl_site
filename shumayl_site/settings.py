25cf5fd1dec246337ed222fe0d0648b2e55ee34e
Shumayl <83513359+mshumayl@users.noreply.github.com>
2021-08-06 18:28:43 +0800

    fixed allowed hosts

--- shumayl_site/settings.py
+++ shumayl_site/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '1e7=%2-!zgtuwyl#3!j*49$d0-^bbo(k2ylv)6#g2xna=&(fzu'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
