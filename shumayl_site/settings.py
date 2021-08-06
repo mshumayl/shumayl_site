3f87c5d35260ab6c50917f04cc5fbeef2010e9b0
Shumayl <83513359+mshumayl@users.noreply.github.com>
2021-08-06 15:37:29 +0800

    try fix model

--- shumayl_site/settings.py
+++ shumayl_site/settings.py
@@ -84,6 +84,7 @@ WSGI_APPLICATION = 'shumayl_site.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
